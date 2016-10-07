07085fc8ea5e68545a52644cc622c2599dbda9c2
Yahor_Kavaliou <yahor.kavaliou@thomsonreuters.com>
2016-10-07 19:49:25 +0300

    parse text by scenarios

--- src/parser/TxtReader.java
+++ src/parser/TxtReader.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
 
 public class TxtReader {
     private String path;
@@ -13,18 +15,48 @@ public class TxtReader {
         path = file_path;
     }
 
-    public List<String> readFile() throws IOException{
+    public List<List<String>> readFile() throws IOException{
         FileReader fileReader = new FileReader(path);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        String aLine;
-        List<String> textData = new ArrayList<>();
+        String line;
+        boolean counter = false;
 
-        while((aLine = bufferedReader.readLine())!= null){
-            textData.add(aLine);
+        List<String> oneScenario = new ArrayList<>();
+        List<List<String>> allScenarios = new ArrayList<List<String>>();
+
+        while((line = bufferedReader.readLine())!= null){
+            boolean newScenario = line.trim().split(" ", 2)[0].equals("Scenario");
+
+            if(newScenario && !counter){
+                //first scenario in the list was found
+                oneScenario.add(line);
+                counter = true;
+            }
+            else if(!newScenario && counter){
+                //adding lines to the current scenario
+                oneScenario.add(line);
+            }
+            else if(newScenario && counter){
+                //save full current scenario to the list and clear current scenario to save the next
+                allScenarios.add(oneScenario);
+                oneScenario.clear();
+                oneScenario.add(line);
+            }
         }
+        allScenarios.add(oneScenario);
 
         bufferedReader.close();
-        return textData;
+
+        return allScenarios;
+    }
+
+    //этот способ наверное получше:
+    // 1) читается весь файл в строку
+    // 2) строка парсится регуляркой
+    // пока не смог подобрать нормальную регулярку, приходется использовать г%вно-метод выше
+    public List<documentObject> readFullFile() throws IOException{
+        String text = new String(readAllBytes(get(path)));
+        return null;
     }
 }
--- src/parser/main.java
+++ src/parser/main.java
@@ -1,15 +1,16 @@
 package parser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class main {
     public static void main(String[] args) {
-        String path = "D:/test.txt";
+        String path = "D:/test2.txt";
 
         try{
             TxtReader txtReader = new TxtReader(path);
-            List<String> result = txtReader.readFile();
+            List<List<String>> result = txtReader.readFile();
 
             int size = result.size();
         }
